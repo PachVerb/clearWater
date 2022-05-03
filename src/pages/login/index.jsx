b8d26f7af26b2cb68420aa1f362d5aba5832f96e
wangshan <22212@qq.com>
2022-05-04 02:06:31 +0800

    feat: 设备适配

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,22 +2,82 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 /*
- * @LastEditTime: 2022-05-04 00:48:23
+ * @LastEditTime: 2022-05-04 01:34:40
  * @Description:
  * @Date: 2022-04-18 18:17:53
  * @Author: wangshan
  * @LastEditors: wangshan
  */
-import { Button } from 'antd'
-export default function () {
-    const location = useLocation()
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 
-    // useEffect(() => {
-    //     console.log(location)
-    // })
+import '@/pages/login/login.scss'
+
+const NormalLoginForm = () => {
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values)
+    }
+
     return (
-        <div>
-            <Button type="primary">Primary Button</Button>
-        </div>
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+                remember: true
+            }}
+            onFinish={onFinish}
+        >
+            <Form.Item
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username!'
+                    }
+                ]}
+            >
+                <Input
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Username"
+                />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!'
+                    }
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a className="login-form-forgot" href="">
+                    Forgot password
+                </a>
+            </Form.Item>
+
+            <Form.Item>
+                <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                >
+                    Log in
+                </Button>
+                Or <a href="">register now!</a>
+            </Form.Item>
+        </Form>
     )
 }
+
+export default () => <NormalLoginForm />
